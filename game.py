a3a5f54a95beddee730a2d3bc00e5d126e6e18f3
David Jablonski <dayjaby@gmail.com>
2019-02-08 12:07:48 +0100

    several improvements

--- game.py
+++ game.py
@@ -1,8 +1,9 @@
-#import the pygame module, and the
+    #import the pygame module, and the
 #sys module for exiting the window we create
 import pygame, sys
 import math
 import numpy as np
+from collections import defaultdict
 
 #import some useful constants
 # see http://www.raspberry-pi-geek.com/Archive/2014/05/Pygame-modules-for-interactive-programs
@@ -19,52 +20,98 @@ WIDTH, HEIGHT = SCREENSIZE = (1200,800)
 DISPLAYSURF = pygame.display.set_mode(SCREENSIZE)
 pygame.display.set_caption('My cool game')
 
-tilemap = pygame.image.load("tilemap.png")
-scrollx, scrolly = (200, 0)
+class Tile:
+    def __init__(self, size, offset):
+        self.size = np.array(size)
+        self.offset = offset
+        self.disabled = False
 
-def transform(pos):
-    px, py = pos
-    y = (2*py - px - 2*scrolly+scrollx-32)/64.0
-    x = (py - 16*y - scrolly-32)/16.0
-    return int(x),int(y)
 
-tiles = np.zeros((LAYERS,MAPWIDTH,MAPHEIGHT))
-holding = [False]*300
-tiles[1,:,:] = 9
+class Tilemap:
+    def __init__(self, image, tile_count, tile_size, tile_offset):
+        self.image = pygame.image.load("tilemap.png")
+        self.tile_count = tile_count
+        self.tile_size = np.array(tile_size)
+        self.tiles = defaultdict(lambda: Tile(tile_size, tile_offset))
+
+    def add_special_tile(self, tile, size, offset):
+        for x in range(size[0]):
+            for y in range(size[1]):
+                dtile = self.get_x(tile) + x + (self.get_y(tile) + y) * self.tile_count[0]
+                print(dtile)
+                self.tiles[dtile].disabled = True
+
+        self.tiles[tile] = Tile(self.tile_size * size, offset)
+
+    def get_offset(self, tile):
+        return self.tiles[tile].offset
+
+    def get_x(self, tile):
+        return tile%self.tile_count[0]
+
+    def get_y(self, tile):
+        return tile/self.tile_count[0]
+
+    def get_rect(self, tile):
+        size = self.tiles[tile].size
+        return Rect(
+            self.get_x(tile)*self.tile_size[0],
+            self.get_y(tile)*self.tile_size[1],
+            size[0], size[1])
+
+tilemap = Tilemap(pygame.image.load("tilemap.png"), (10,17), (64,64), (32,32))
+tilemap.add_special_tile(137, (4,3), (96,112))
+tilemap.add_special_tile(134, (3,3), (96,132))
+scroll = [200, 0]
+
+a = np.array([[32, -32], [16, 16]])
+ainv = np.linalg.inv(a)
+positions = np.dot(a, np.indices(MAPSIZE).reshape((2, MAPWIDTH * MAPHEIGHT))).reshape((2, MAPWIDTH, MAPHEIGHT))
+
+current_map = np.zeros((LAYERS,MAPWIDTH,MAPHEIGHT))
+current_map[1,:,:] = 9
 current_layer = 1
 
+running = True
 #loop (repeat) forever
-while True:
+while running:
     #get all the user events
     for event in pygame.event.get():
         #if the user wants to quit
         if event.type == QUIT:
-            #end the game and close the window
-            print("quit")
-            pygame.quit()
-            sys.exit()
+            running = False
         elif event.type == KEYDOWN:
-            holding[event.key] = True
+            if event.key == K_ESCAPE:
+                running = False
+            pass
         elif event.type == KEYUP:
-            holding[event.key] = False
+            pass
+        elif event.type == MOUSEBUTTONDOWN:
+            pass
         elif event.type == MOUSEBUTTONUP:
-            x,y = transform(event.pos)
-            tiles[current_layer,x,y] = 50
+            x,y = (np.dot(ainv, np.array(event.pos) - scroll)).astype(int)
+            if x >= 0 and x < MAPWIDTH and y >= 0 and y < MAPHEIGHT:
+                current_map[current_layer,x,y] = 134
         else:
             pass
-    
-    if holding[K_LEFT] == 1:
-        scrollx+=5
-    elif holding[K_RIGHT] == 1:
-        scrollx-=5
+
+    pressed = pygame.key.get_pressed()
+    if pressed[K_LEFT] == 1:
+        scroll[0]+=10
+    elif pressed[K_RIGHT] == 1:
+        scroll[0]-=10
     for layer in range(0,LAYERS):
         for x in range(MAPWIDTH):
             for y in range(MAPHEIGHT):
-                DISPLAYSURF.blit(tilemap, 
-                    dest=(32*x-32*y+scrollx,16*x+16*y+scrolly), 
-                    area=Rect((tiles[layer,x,y]%10)*64,(tiles[layer,x,y]/10)*64,64,64))
+                #pos = np.dot(a, np.array([[x], [y]]))
+                tile = current_map[layer, x, y]
+                DISPLAYSURF.blit(tilemap.image,
+                    dest=positions[:,x,y] + scroll - tilemap.get_offset(tile),
+                    area=tilemap.get_rect(tile))
 
-    #update the display      
+    #update the display
     pygame.display.update()
     DISPLAYSURF.fill((0,0,0))
 
+pygame.quit()
+sys.exit()
